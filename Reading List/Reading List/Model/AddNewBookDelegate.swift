9daccd76d0360347ce2258ae3b2dd6c94f76d1a3
Marc <marc.daniel.jacques@gmail.com>
2021-03-12 12:53:41 -0500

    - Added delegate to have readingListTVC reload data after each book is added

---
+++ Reading List/Reading List/Model/AddNewBookDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AddNewBookDelegate.swift
+//  Reading List
+//
+//  Created by Marc Jacques on 3/12/21.
+//  Copyright © 2021 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+protocol AddNewBookDelegate {
+    
+    func newBookAdded(book: Book)
+}
